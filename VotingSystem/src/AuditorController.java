9e43ad43a991f2f6738431c90ffedc541978ef16
Sophia Mallaro <sophia-mallaro@uiowa.edu>
2016-12-03 09:32:43 -0600

    got precincts working

--- VotingSystem/src/AuditorController.java
+++ VotingSystem/src/AuditorController.java
@@ -2,6 +2,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 
@@ -16,16 +17,21 @@ import java.util.ResourceBundle;
  * Created by smallaro on 12/2/16.
  */
 public class AuditorController implements Initializable{
-    private char[] precinctCode;
     private TestDB data = new TestDB();
     private ObservableList precinctList;
     List<State> states;
+    private String idCode;
+    private String stateID;
+    private String precinctID;
 
     @FXML
     ComboBox<String> stateSelect;
 
     @FXML
     ComboBox<String> precinct;
+
+    @FXML
+    Button generateButton;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -35,54 +41,41 @@ public class AuditorController implements Initializable{
             stateNames.add(state.toString());
         }
         ObservableList obList = FXCollections.observableList(stateNames); //http://stackoverflow.com/questions/18361195/javafx-how-to-load-populate-values-at-start-up
-
         List<String> test = new ArrayList<>();
         ObservableList precinctList = FXCollections.observableList(test);
         stateSelect.setItems(obList);
-
         precinct.setItems(precinctList);
-
-    }//hello
+    }
 
 
     @FXML
     public void stateSelected() throws Exception{
         for(State state : states) {
             if(state.getStateName().equals(stateSelect.getValue())) {
-                List<Precinct> precincts = data.getPrecincts(state.getId());
+                int id = state.getId();
+                stateID = Integer.toString(id);
+                List<Precinct> precincts = data.getPrecincts(id);
                 List<String> precinctNames = new ArrayList<>();
                 for(Precinct precinct : precincts) {
                     precinctNames.add(precinct.getId());
                 }
-
                 ObservableList ob = FXCollections.observableList(precinctNames);
                 precinct.setItems(ob);
             }
         }
-
-
-        //System.out.println("Triggered");
-        //List<Precinct> precincts = data.getPrecincts(data.getStateID(stateSelect.getSelectionModel().getSelectedItem().toString()));
-        //List<Precinct> precincts = data.getPrecincts(data.getStateID( stateSelect.getValue()));
-        //ObservableList ob = FXCollections.observableList(precincts); //http://stackoverflow.com/questions/18361195/javafx-how-to-load-populate-values-at-start-up
-        //precinct.setItems(ob);
-        /*List<String> testList = new ArrayList<>();
-        testList.add("Hello");
-        testList.add("It's me");
-        //ObservableList ob = FXCollections.observableList(testList);
-        //precinct.setItems(ob);
-        precinctList = FXCollections.observableList(testList);
-        precinct.setItems(precinctList); */
-
-
     }
 
-    /*@FXML
-    public void buttonPressed() {
-        List<Precinct> precincts = data.getPrecincts(data.getStateID( stateSelect.getValue()));
-        precinct = new ChoiceBox<String>();
-        ObservableList ob = FXCollections.observableList(precincts); //http://stackoverflow.com/questions/18361195/javafx-how-to-load-populate-values-at-start-up
-        precinctSelect.setItems(ob);
-    }*/
+    @FXML
+    public void precinctSelected() throws Exception{
+        precinctID = precinct.getValue();
+    }
+
+    @FXML
+    public void buttonPressed() throws Exception { //Generates full id code for precinct
+        idCode = stateID + precinctID;
+        if (idCode.length() == 3) {
+            idCode = "0" + idCode;
+        }
+    }
 
 }
