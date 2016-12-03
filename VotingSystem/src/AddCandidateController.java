61c96c764953f6ea7314426b3bc72c724a42227b
Tengku T Mohd <tengku-tmohd@uiowa.edu>
2016-12-03 13:54:55 -0600

    controller for adding candidates

--- VotingSystem/src/AddCandidateController.java
+++ VotingSystem/src/AddCandidateController.java
@@ -13,6 +13,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +24,6 @@ import java.util.ResourceBundle;
 public class AddCandidateController implements Initializable {
     List<Position> positions;
     private TestDB data = new TestDB();
-    private String partySelected;
     Candidate newCandidate = new Candidate();
 
     @FXML
@@ -45,25 +45,41 @@ public class AddCandidateController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         partySelect.getItems().clear();
         partySelect.getItems().addAll("Democrat", "Republican", "Green Party", "Independent", "Libertarian");
+        List<Position>  offices = data.getPositions("1502");
+        List<String> titleNames = new ArrayList<>();
+        for(Position position : offices) {
+            titleNames.add(position.getPositiontitle());
+        }
+
+        positionSelect.getItems().addAll(titleNames);
     }
 
     @FXML
     public void partySelected() throws Exception {
-        newCandidate.setFirstName(candidateFirstName.getText());
-        newCandidate.setLastName(candidateLastName.getText());
         newCandidate.setParty(partySelect.getValue().toString());
-        positionSelect.getItems().addAll("President", "Vice President", "House of Representative");
     }
 
     @FXML
     public void positionSelected() throws Exception {
-        partySelected = partySelect.getValue().toString();
-        int positionID = data.getPositionID(partySelected);
+        String selected = partySelect.getValue().toString();
+        Integer positionID = data.getPositionID(selected);
         newCandidate.setPositionid(positionID);
     }
 
     @FXML
-    public void submitButtonPressed(ActionEvent event) throws Exception {
-        data.addCandidate(newCandidate);
+    public void submitButtonPressed() throws Exception {
+
+        try {
+            newCandidate.setFirstName(candidateFirstName.getText());
+            newCandidate.setLastName(candidateLastName.getText());
+            data.addCandidate(newCandidate);
+        }
+        catch (NullPointerException ex)  {
+            candidateFirstName.setText("Enter First Name");
+            candidateFirstName.selectAll();
+            candidateLastName.setText("Enter Last Name");
+            candidateLastName.selectAll();
+
+        }
     }
 }
