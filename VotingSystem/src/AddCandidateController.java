611df1b1463be7564b4094af2201a5df0caf5092
Tengku T Mohd <tengku-tmohd@uiowa.edu>
2016-12-03 11:03:24 -0600

    controller for adding candidates

--- VotingSystem/src/AddCandidateController.java
+++ VotingSystem/src/AddCandidateController.java
@@ -3,26 +3,48 @@
  */
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class AddCandidateController {
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+import java.util.ResourceBundle;
+import java.util.ResourceBundle;
+
+public class AddCandidateController implements Initializable{
+    private String candidate_firstName;
+    private String candidate_lastName;
+    List<String> party;
+
 
     @FXML
-    private TextField candidateFirstName;
+    TextField candidateFirstName;
 
     @FXML
-    private TextField getCandidateLastName;
+    TextField candidateLastName;
 
     @FXML
-    private ComboBox partySelect;
+    ComboBox partySelect;
 
     @FXML
-    private ComboBox positionSelect;
+    ComboBox positionSelect;
 
-    public void initialize()    {
+    @Override
+    public void initialize(URL url, ResourceBundle rb)    {
+
+    }
+
+    @FXML
+    public void firstNameEntered() throws Exception   {
 
     }
 
