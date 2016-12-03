8d3ced5aff349b0e6a4f6988b4c8df741ae96cea
Tengku T Mohd <tengku-tmohd@uiowa.edu>
2016-12-03 17:35:32 -0600

    java docs and inline comments

--- VotingSystem/src/Candidate.java
+++ VotingSystem/src/Candidate.java
@@ -21,12 +21,12 @@ public class Candidate {
     }
 
     /**
+     * Constructor with the parameters of three Strings and one integer.
      *
-     *
-     * @param firstName
-     * @param lastName
-     * @param party
-     * @param positionid
+     * @param firstName The candidate's first name
+     * @param lastName The candidate's last name
+     * @param party The candidate's party
+     * @param positionid The candidate's position ID in the database
      */
     public Candidate(String firstName, String lastName, String party, int positionid) {
         this.firstName = firstName;
@@ -37,13 +37,14 @@ public class Candidate {
     }
 
     /**
+     * Constructor with the parameter of three integers and three Strings
      *
-     * @param candidateid
-     * @param firstName
-     * @param lastName
-     * @param party
-     * @param voteCount
-     * @param positionid
+     * @param candidateid The candidate's ID in the database
+     * @param firstName The candidate's first name
+     * @param lastName The candidate's last name
+     * @param party The candidate's party
+     * @param voteCount The candidate's vote count
+     * @param positionid The candidate's position in the database
      */
     public Candidate(int candidateid, String firstName, String lastName, String party, int voteCount, int positionid) {
         this.firstName = firstName;
@@ -56,8 +57,8 @@ public class Candidate {
     }
 
     /**
-     *
-     * @return
+     * An accessor method to get the candidate's ID
+     * @return the candidate's ID in the database
      */
     public int getCandidateid() {
         return candidateid;
@@ -84,63 +85,63 @@ public class Candidate {
     }
 
     /**
-     *
-     * @return
+     * An accessor method to get the last name of the candidate
+     * @return the last name of the candidate
      */
     public String getLastName() {
         return lastName;
     }
 
     /**
-     *
-     * @param lastName
+     * An update method to set the last name of the candidate
+     * @param lastName candidate's last name
      */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
     /**
-     *
-     * @return
+     * An accessor method to get the candidate's party
+     * @return the candidate's party affiliation
      */
     public String getParty() {
         return party;
     }
 
     /**
-     *
-     * @param party
+     * An update method to set the candidate's party
+     * @param party candidate's party
      */
     public void setParty(String party) {
         this.party = party;
     }
 
     /**
-     *
-     * @return
+     * An accessor method to get the number of votes for the candidate
+     * @return the number of votes
      */
     public int getVoteCount() {
         return voteCount;
     }
 
     /**
-     *
-     * @param voteCount
+     * An update method to set the number of votes
+     * @param voteCount the number of votes
      */
     public void setVoteCount(int voteCount) {
         this.voteCount = voteCount;
     }
 
     /**
-     *
-     * @return
+     * An accessor method to get the position ID of the candidate in the database
+     * @return the candidate's position ID
      */
     public int getPositionid() {
         return positionid;
     }
 
     /**
-     *
+     * An update method to set the position ID of the candidate in the database
      * @param positionid
      */
     public void setPositionid(int positionid) {
