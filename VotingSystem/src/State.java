e8a2d3b30a34bd72f7762119fb55fd9d21998ff0
Tengku T Mohd <tengku-tmohd@uiowa.edu>
2016-12-03 19:20:09 -0600

    java docs and inline comments

--- VotingSystem/src/State.java
+++ VotingSystem/src/State.java
@@ -1,34 +1,65 @@
 /**
+ * The State class is a class that stores the details regarding
+ * for database use.
  *
  * @author Sophia Mallaro
  * Created by smallaro on 12/2/16.
  *
  */
 public class State {
+    //Instantiation of variables
     private int id;
     private String stateName;
 
+    /**
+     * Constructor with two parameters: an integer and a String
+     * @param id state ID in the database
+     * @param stateName The State's Name
+     */
     public State(int id, String stateName) {
         this.id = id;
         this.stateName = stateName;
     }
 
+    /**
+     * Accessor method that returns the state ID in the database
+     * @return the state ID
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Update method to set the state ID to the integer passed in
+     * the argument
+     * @param id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Accessor method that returns the state's name
+     * @return the state's name
+     */
     public String getStateName() {
         return stateName;
     }
 
+    /**
+     * Update method that set the state name to the String passed in
+     * the argument
+     * @param stateName
+     */
     public void setStateName(String stateName) {
         this.stateName = stateName;
     }
 
+    /**
+     * Overriden method of converting the state name to
+     * a String.
+     * @return A String of the state's name.
+     */
     @Override
     public String toString() {
         return stateName;
