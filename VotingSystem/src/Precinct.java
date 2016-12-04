042276b7278753207f45b6539377d2658f570e8f
Tengku T Mohd <tengku-tmohd@uiowa.edu>
2016-12-03 19:41:07 -0600

    java docs and inline comments for Precinct class

--- VotingSystem/src/Precinct.java
+++ VotingSystem/src/Precinct.java
@@ -1,4 +1,10 @@
 /**
+ * This Precinct class stores the details of the precinct and
+ * contains methods to manipulate the member variables. The state ID
+ * stores the state ID of the state that corresponds to the
+ * precinct and the id member variable stores the precinct ID for
+ * the database record.
+ *
  * Created by smallaro on 12/2/16.
  */
 public class Precinct {
