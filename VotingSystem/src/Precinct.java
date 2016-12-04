0c127613233abd922b6bef92dd3189c8eea11f8c
Tengku T Mohd <tengku-tmohd@uiowa.edu>
2016-12-03 19:43:46 -0600

    java docs and inline comments for Precinct class

--- VotingSystem/src/Precinct.java
+++ VotingSystem/src/Precinct.java
@@ -5,21 +5,39 @@
  * precinct and the id member variable stores the precinct ID for
  * the database record.
  *
+ * @author Sophia Mallaro
  * Created by smallaro on 12/2/16.
  */
 public class Precinct {
+
+    //Instantiation of member variables
     private int stateid;
     private String id;
 
+    /**
+     * Constructor with two parameters
+     * @param stateid the state ID of the state the precinct is in
+     * @param id the precinct ID
+     */
     public Precinct(int stateid, String id) {
         this.stateid = stateid;
         this.id = id;
     }
 
+    /**
+     * Accessor method that returns the state ID of the state
+     * the precinct is in
+     *
+     * @return the stateID of the precinct's state
+     */
     public int getStateid() {
         return stateid;
     }
 
+    /**
+     * Update method of the
+     * @param stateid
+     */
     public void setStateid(int stateid) {
         this.stateid = stateid;
     }
